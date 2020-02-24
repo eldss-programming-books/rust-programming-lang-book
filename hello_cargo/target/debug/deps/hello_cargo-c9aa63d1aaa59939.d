8380715ba8c014dd5f3b2a3b2b4df66629685bde
Evan <EvanLDouglass@gmail.com>
2020-02-23 18:39:45 -0800

    remove target files, add gitignore

--- hello_cargo/target/debug/deps/hello_cargo-c9aa63d1aaa59939.d
+++
@@ -1,5 +0,0 @@
-/home/evan/Code/rust/learn/rust_programming_language_book/hello_cargo/target/debug/deps/hello_cargo-c9aa63d1aaa59939: src/main.rs
-
-/home/evan/Code/rust/learn/rust_programming_language_book/hello_cargo/target/debug/deps/hello_cargo-c9aa63d1aaa59939.d: src/main.rs
-
-src/main.rs:
--- hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/home/evan/Code/rust/learn/rust_programming_language_book/hello_cargo/target/debug/hello_cargo: /home/evan/Code/rust/learn/rust_programming_language_book/hello_cargo/src/main.rs
