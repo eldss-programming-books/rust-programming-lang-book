78906aa2c5a4bc165cd48f000508675f83c7a773
Evan <EvanLDouglass@gmail.com>
2020-02-23 18:38:00 -0800

    add hello_cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-9678e9919e0d829e.d
@@ -0,0 +1,5 @@
+/home/evan/Code/rust/learn/rust_programming_language_book/hello_cargo/target/debug/deps/hello_cargo-9678e9919e0d829e.rmeta: src/main.rs
+
+/home/evan/Code/rust/learn/rust_programming_language_book/hello_cargo/target/debug/deps/hello_cargo-9678e9919e0d829e.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/evan/Code/rust/learn/rust_programming_language_book/hello_cargo/target/debug/hello_cargo: /home/evan/Code/rust/learn/rust_programming_language_book/hello_cargo/src/main.rs
