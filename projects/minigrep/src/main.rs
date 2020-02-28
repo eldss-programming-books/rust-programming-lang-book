12064a1c7ab4973ec0fdf6c84aa54e79ec36e57d
Evan <EvanLDouglass@gmail.com>
2020-02-28 00:17:46 -0800

    Create a mini-grep program to demonstrate concepts learned so far.

---
+++ projects/minigrep/src/main.rs
@@ -0,0 +1,23 @@
+use std::env;
+use std::process;
+
+use minigrep::Config;
+
+fn main() {
+    // Get command line arguments and put into a Vector
+    // To accept non Unicode args, use std::env::args_os
+    let args: Vec<String> = env::args().collect();
+
+    // Store vars
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    // Run the program
+    // If error occurs print message and stop
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    }
+}
